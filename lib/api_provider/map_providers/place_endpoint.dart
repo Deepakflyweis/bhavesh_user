9909a646db932f9d8c8010c8a166c6b587ec9df1
preet3001 <preetsingh3001@gmail.com>
2022-04-15 18:36:33 +0530

    update

--- lib/api_provider/map_providers/place_endpoint.dart
+++ lib/api_provider/map_providers/place_endpoint.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:we_fast/api_provider/map_client.dart';
 import 'package:we_fast/constants/env.dart';
+import 'package:we_fast/models/places_model.dart';
 
 class PlaceEndPointProvider {
   Dio client;
@@ -12,9 +15,13 @@ class PlaceEndPointProvider {
     try {
       Response r = await client
           .get("/place/textsearch/json?query=$searchText&key=$googleMapApiKey");
-      if (r.statusCode == 200) {}
-    } on Exception catch (e) {
-      // TODO
+      if (r.statusCode == 200) {
+        return placesModelFromJson(jsonEncode(r.data));
+      } else {
+        return Future.error(r.data["status"]);
+      }
+    } on DioError catch (e) {
+      return Future.error(e.message);
     }
   }
 }
--- lib/constants/env.dart
+++ lib/constants/env.dart
@@ -1,3 +1,3 @@
 const String googleMapApiKey =
-    "AIzaSyDj1xhHpQ-eM8Kx0WkcS4kR7Zq3UKRGhbg";//if changing key also replace in mannifest
+    "AIzaSyDQ__1W3C2GYf95cWNIN9g3AjPbgpFaev0";//if changing key also replace in mannifest
 
